efcc6f37baa3e3b53fa04228b284638b3fa513c7
calvinodev <36681556+calvinodev@users.noreply.github.com>
2018-03-15 20:05:22 -0400

    Update arduos.ino

--- Arduino Software/arduos.ino
+++ Arduino Software/arduos.ino
@@ -17,9 +17,10 @@
 #include <Adafruit_SSD1331.h>
 #include <SPI.h>
 
+
 Adafruit_SSD1331 display = Adafruit_SSD1331(cs, dc, mosi, sclk, rst);  
 
-float p = 3.1415926;
+float pie = 3.1415926;
 
 void setup(void) {
   Serial.begin(9600);            //Start serial
@@ -28,9 +29,9 @@ void setup(void) {
   display.fillScreen(BLACK);     //Clear the screen
 }
 
-int seconds   = 0;               //The second
-int minutes   = 0;               //The minute
-int hours     = 0;               //The hour
+int seconds   = 10;               //The second
+int minutes   = 10;               //The minute
+int hours     = 10;               //The hour
 bool am       = true;            //Is it AM or PM
 
 String BTCVAL = "$00000.00";     //Value of Bitcoin to be recieved from BLE   
@@ -40,16 +41,29 @@ String msg2     = "";            //2st line message to be displayed from BLE
 String msg3     = "";            //3st line message to be displayed from BLE
 String instring = "";            //Whole imput string from BLE
 
+int clockCenterX = 48;           //Clock center x
+int clockCenterY = 32;           //Clock center y
+
+String clockface = "2";           //What clockface is active
+
 void loop() {  
   Serial.begin(9600);                      //Start serial
   if(Serial.available()) {                 //If message is recieved from serial (BLE)
     delay(500);                            //Wait for full message (.5 seconds)
     GetSerialBLE();                        //Split Message Recieved from Serial(BLE) into HOUR, MINUTE, SECOND, MSG(1,2,3), and BTCVAL
-    DisplayMessage(msg1, msg2, msg3, 5);   //Display the recieved data
+    //DisplayMessage(msg1, msg2, msg3, 5);   //Display the recieved data
   }
   UpdateTime();                            //Update the time
-  //DisplayTimeDig(5);                     //Display the time (Numbers)
-  DisplayTimeClock();                      //Display the time (Clock)
+  if(clockface = "1") {                    //Is clock face digital
+    DisplayTimeDig(5);                     //Display the time (Numbers)
+  }
+  if(clockface = "2") {                    //Is clock face traditional
+    DisplayTimeClock();                    //Display the time (Clock)
+  } 
+  Serial.println(String(minutes));
+  delay(10);
+  Serial.println(clockface);
+  delay(10);
 }
 
 void DisplayMessage (String m1, String m2, String m3, int delayt)    //Display a message
@@ -121,6 +135,16 @@ void GetSerialBLE ()                                                 //Get and s
         BTCVAL = instring.substring(i+1, i+10);                      //Read just the BTC text and set to BTCVAL
         instring.remove(i+1, i+10);                                  //Cut out the BTC text
       }
+
+      if(instring.substring(i, i+1) == "C")                         //If the characters C is found
+      {
+        clockface = "2";                                            //Set clockface to 2
+      }
+      
+      if(instring.substring(i, i+1) == "D")                         //If the characters D is found
+      {
+        clockface = "1";                                            //Set clockface to 2
+      }
     }
 
     Serial.println(BTCVAL);
@@ -146,6 +170,8 @@ void UpdateTime ()          //Update the time
       am = true;           //Set the time to AM
     } 
   }
+
+  seconds++;
 }
 
 void DisplayTimeDig (int delaytime)        //Display the time on screen
@@ -196,34 +222,125 @@ void DisplayTimeDig (int delaytime)        //Display the time on screen
 
 void DisplayTimeClock ()
 {
-  int outlinemin[2] = {0, 0};                                       //Position on the circumfrence of clock circle for minute line
-  int outlinehor[2] = {0, 0};                                       //Position on the circumfrence of clock circle for hours line
-  int outlinesec[2] = {0, 0};                                       //Position on the circumfrence of clock circle for seconds line
 
-  int minangle = 0;                                                 //Angle for minute hand
-  int horangle = 0;                                                 //Angle for hours hand
-  int secangle = 0;                                                 //Angle for second hand
 
-  minangle = 360/minutes;                                           //Calculate angle for minutes
-  horangle = 360/hours;                                             //Calculate angle for minutes
-  secangle = 360/seconds;                                           //Calculate angle for minutes
-
-  outlinemin[0] = 27*cos(minangle)+48;                              //Calculate x position for minute hand
-  outlinemin[1] = 27*cos(minangle)+32;                              //Calculate y position for minute hand 
-  outlinehor[0] = 15*cos(horangle)+48;                              //Calculate x position for minute hand
-  outlinehor[1] = 15*cos(horangle)+32;                              //Calculate y position for minute hand
-  outlinesec[0] = 21*cos(secangle)+48;                              //Calculate x position for minute hand
-  outlinesec[1] = 21*cos(secangle)+32;                              //Calculate y position for minute hand
+  for (int i=0; i<2; i++)
+  {
+    display.drawCircle(clockCenterX, clockCenterY, 31-i, WHITE);
+  }
+  for (int i=0; i<3; i++)
+  {
+    display.drawCircle(clockCenterX, clockCenterY, i, WHITE);
+  }
   
-  display.drawCircle(48,32,30,WHITE);                               //Draws clock outside
-  display.drawLine(outlinemin[0], outlinemin[1], 48, 32, WHITE);    //Draws minute line
-  display.drawLine(outlinehor[0], outlinehor[1], 48, 32, WHITE);    //Draws hour line
-  display.drawLine(outlinesec[0], outlinesec[1], 48, 32, RED);      //Draws seconds line
+  // Draw a small mark for every hour
+  for (int i=0; i<12; i++)
+  {
+    drawMark(i, WHITE);
+  }
 
-  delay(1);
+  drawSec(seconds, RED);
+  drawMin(minutes, WHITE);
+  drawHour(hours, minutes, WHITE);
 
-  display.drawLine(outlinemin[0], outlinemin[1], 48, 32, BLACK);    //Erases minute line
-  display.drawLine(outlinehor[0], outlinehor[1], 48, 32, BLACK);    //Erases hour line
-  display.drawLine(outlinesec[0], outlinesec[1], 48, 32, BLACK);      //Erases seconds line  
+  delay(2500);
+
+
+  for (int i=0; i<2; i++)
+  {
+    display.drawCircle(clockCenterX, clockCenterY, 31-i, BLACK);
+  }
+  for (int i=0; i<3; i++)
+  {
+    display.drawCircle(clockCenterX, clockCenterY, i, BLACK);
+  }
   
+  // Draw a small mark for every hour
+  for (int i=0; i<12; i++)
+  {
+    drawMark(i, BLACK);
+  }
+
+  drawSec(seconds, BLACK);
+  drawMin(minutes, BLACK);
+  drawHour(hours, minutes, BLACK);
 }
+
+
+void drawMark(int h, uint16_t col)
+{
+  float x1, y1, x2, y2;
+  
+  h=h*30;
+  h=h+270;
+  
+  x1=29*cos(h*0.0175);
+  y1=29*sin(h*0.0175);
+  x2=26*cos(h*0.0175);
+  y2=26*sin(h*0.0175);
+  
+  display.drawLine(x1+clockCenterX, y1+clockCenterY, x2+clockCenterX, y2+clockCenterY, col);
+}
+
+void drawSec(int s, uint16_t col)
+{
+  float x1, y1, x2, y2;
+
+  s=s*6;
+  s=s+270;
+  
+  x1=34*cos(s*0.0175);
+  y1=34*sin(s*0.0175);
+  x2=26*cos(s*0.0175);
+  y2=26*sin(s*0.0175);
+  
+  //if ((s % 5) == 0)
+  //  display.drawLine(x1+clockCenterX, y1+clockCenterY, x2+clockCenterX, y2+clockCenterY, BLACK);
+  //else
+    display.drawLine(x1+clockCenterX, y1+clockCenterY, x2+clockCenterX, y2+clockCenterY, col);
+}
+
+void drawMin(int m, uint16_t col)
+{
+  float x1, y1, x2, y2, x3, y3, x4, y4;
+
+  m=m*6;
+  m=m+270;
+  
+  x1=25*cos(m*0.0175);
+  y1=25*sin(m*0.0175);
+  x2=3*cos(m*0.0175);
+  y2=3*sin(m*0.0175);
+  x3=10*cos((m+8)*0.0175);
+  y3=10*sin((m+8)*0.0175);
+  x4=10*cos((m-8)*0.0175);
+  y4=10*sin((m-8)*0.0175);
+  
+  display.drawLine(x1+clockCenterX, y1+clockCenterY, x3+clockCenterX, y3+clockCenterY, col);
+  display.drawLine(x3+clockCenterX, y3+clockCenterY, x2+clockCenterX, y2+clockCenterY, col);
+  display.drawLine(x2+clockCenterX, y2+clockCenterY, x4+clockCenterX, y4+clockCenterY, col);
+  display.drawLine(x4+clockCenterX, y4+clockCenterY, x1+clockCenterX, y1+clockCenterY, col);
+}
+
+void drawHour(int h, int m, uint16_t col)
+{
+  float x1, y1, x2, y2, x3, y3, x4, y4;
+
+  h=(h*30)+(m/2);
+  h=h+270;
+  
+  x1=20*cos(h*0.0175);
+  y1=20*sin(h*0.0175);
+  x2=3*cos(h*0.0175);
+  y2=3*sin(h*0.0175);
+  x3=8*cos((h+12)*0.0175);
+  y3=8*sin((h+12)*0.0175);
+  x4=8*cos((h-12)*0.0175);
+  y4=8*sin((h-12)*0.0175);
+  
+  display.drawLine(x1+clockCenterX, y1+clockCenterY, x3+clockCenterX, y3+clockCenterY, col);
+  display.drawLine(x3+clockCenterX, y3+clockCenterY, x2+clockCenterX, y2+clockCenterY, col);
+  display.drawLine(x2+clockCenterX, y2+clockCenterY, x4+clockCenterX, y4+clockCenterY, col);
+  display.drawLine(x4+clockCenterX, y4+clockCenterY, x1+clockCenterX, y1+clockCenterY, col);
+}
+
